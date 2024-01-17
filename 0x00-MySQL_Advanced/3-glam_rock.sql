1026664c4f0e75572457906cbdd58f24cb64a671
mohamad-samir <mohamad.samir2011@gmail.com>
2024-01-17 19:48:47 +0200

    update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
---  lists all bands with Glam rock as their main style, ranked by their longevity
+--lists all bands with Glam rock as their main style,ranked by their longevity
 SELECT band_name,
        IFNULL(2022 - formed, 0) AS lifespan
 FROM metal_bands
