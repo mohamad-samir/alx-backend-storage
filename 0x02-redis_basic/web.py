0705c2934c72797129c53adb6f176b931ac9e8a9
mohamad-samir <mohamad.samir2011@gmail.com>
2024-01-24 21:09:18 +0200

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -43,11 +43,25 @@ def url_access_count(method):
     return wrapper
 
 
-@url_access_count
-def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
-    results = requests.get(url)
-    return results.text
+def url_access_count(method):
+    """decorator for get_page function"""
+    @wraps(method)
+    def wrapper(url):
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+        # Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        # Set expiration during the set operation
+        r.set(key, html_content, ex=10)
+        return html_content
+    return wrapper
 
 
 if __name__ == "__main__":
