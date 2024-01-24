c22b2d2068ef64c2285695c4ebcc4f7e074b70d4
mohamad-samir <mohamad.samir2011@gmail.com>
2024-01-24 21:00:33 +0200

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -104,13 +104,14 @@ class Cache:
         value = self._redis.get(key)
         return value.decode('utf-8')
 
-    def get_int(self, key: str) -> int:
-        '''
-            Get an int from the cache.
-        '''
-        value = self._redis.get(key)
-        try:
-            value = int(value.decode('utf-8'))
-        except Exception:
-            value = 0
-        return value
+
+def get_int(self, key: str) -> int:
+    '''
+    Get an int from the cache.
+    '''
+    value = self._redis.get(key)
+    try:
+        value = int(value.decode('utf-8'))
+    except (ValueError, TypeError):
+        value = 0
+    return value
