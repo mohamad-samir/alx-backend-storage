30cd43d1a576a580cf86d1a1343ec9264d84ba22
mohamad-samir <mohamad.samir2011@gmail.com>
2024-01-24 21:19:29 +0200

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
-"""Implement an expiring web cache and tracker"""
-from datetime import timedelta
-from typing import Callable
-import requests
+""" Redis Module Cache and tracking """
+
 from functools import wraps
 import redis
+import requests
+from typing import Callable
+
+redis_ = redis.Redis()
 
 
-def cache_track_response(method: Callable):
-    """Cache and track function response"""
-
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
     @wraps(method)
-    def wrapper(url: str, *args, **kwargs):
-        """Implement the caching and tracking"""
-        count_key = "count:{}".format(url)
-        _redis = redis.Redis()
-        _redis.incr(count_key)
-        result = method(url, *args, **kwargs)
-        _redis.setex(url, timedelta(seconds=10), str(result))
-        return result
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
 
     return wrapper
 
 
-@cache_track_response
+@count_requests
 def get_page(url: str) -> str:
-    """Return HTML content of the `url` specified"""
-    response = requests.get(url)
-    return response.text
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
